47f0b7230a50b301c1032b7d19e47d5c07e8003a
Fay Alawadhi <fayalawadhi@Fays-MBP.zain>
2019-10-12 16:59:04 +0300

    DONE

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Page(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now=True)
+
+	def get_absolute_url(self):
+		return reverse("page-detail", args=[self.id])
+
+	def __str__(self):
+		return "TITLE: %s" % self.title
